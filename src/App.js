51c5ae9c8e29706ec3e6bc35d39a3ac02c8eaa88
luka <lukicha.kobaidze@gmail.com>
2020-10-26 18:02:43 +0400

    Test Mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Recipe from './Recipe';
 
-function App() {
+const App = () => {
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('khachapuri')
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const APP_ID = "c9f639f5";
+  const APP_KEY = "2c85416bd70f5b38482ebe963e81c65b";
+
+  const getRecipes = async () => {
+    const respose = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await respose.json();
+    setRecipes(data.hits)
+    console.log(recipes)
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search);
+    setSearch('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className='recipes'>
+      {recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label}
+        ingredients={recipe.recipe.ingredients}
+         calories={recipe.recipe.calories}
+         image={recipe.recipe.image}
+          />
+      ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
